8eb5f6c687344ca869adfd37f348a9adb52648da
jdrichardstech <jdrichardstech@gmail.com>
2017-07-05 16:53:38 -0400

    add routes directory

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
+const routes = require('./routes/index')
 require('dotenv').config()
 
-mongoose.connect(process.env.DB_URL, function(err, res){
+
+
+mongoose.connect(process.env.DB_URL_DEV, function(err, res){
   if(err){
     console.log('DB CONNECTION FAIL: ' + err)
   }else{
-    console.log('DB CONNECTION SUCCESS: ' + process.env.DB_URL)
+    console.log('DB CONNECTION SUCCESS: ' + process.env.DB_URL_DEV)
   }
 })
 
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use('/', routes)
 
-app.get('/', (req, res, next) => {
-	res.status(200).send('Hello Trackbot!')
-})
 
 app.listen(port, ()=>{
 	console.log('Listening on port' + port)
 } )
+module.exports = app;
