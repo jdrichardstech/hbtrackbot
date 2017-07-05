faebb0b839f8dae748b5c24a3e6d504a31f24f66
jdrichardstech <jdrichardstech@gmail.com>
2017-07-05 15:53:19 -0400

    connect mongoose to local server

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.DB_URL, function(err, res){
+  if(err){
+    console.log('DB CONNECTION FAIL: ' + err)
+  }else{
+    console.log('DB CONNECTION SUCCESS: ' + process.env.DB_URL)
+  }
+})
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
